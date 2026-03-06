a1be058fdaebe16d0457e62f8335113dac300f4e
ohseungyeon <alstmd9902@naver.com>
2026-03-06 17:24:55 +0900

    chore: clean up project template and setup global utilities (#7) (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 
+import '@fontsource/pretendard/400.css'
+import '@fontsource/pretendard/500.css'
+import '@fontsource/pretendard/700.css'
 import { createRoot } from 'react-dom/client'
-
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
