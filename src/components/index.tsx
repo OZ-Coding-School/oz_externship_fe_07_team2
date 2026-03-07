510e8c9438b3c1203d788ca19f79f8c85471a816
SammyLee <sammul95@gmail.com>
2026-03-07 19:07:12 +0900

    feat: implement category path (#20)
    * feat: add category path stories (#16)
    
    - Default, Detail, Interactive 스토리작성
    
    * feat: add category path component (#16)
    
    - cva 기반 list/detail variant 작성
    - 마지막 항목 언더라인 처리
    - ChevronRight 아이콘 추가
    - index.ts 배럴패턴 CategoryPath export 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 // components
 export { default as TabButton } from './common/tab-button/TabButton'
 export { default as Avatar } from './common/avatar/Avatar'
+export { default as CategoryPath } from './common/category-path/CategoryPath'
