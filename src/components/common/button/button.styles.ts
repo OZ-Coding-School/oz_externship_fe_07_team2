64d4b0d03926b10a6703ebb58cd170761cb2ad6b
ohseungyeon <alstmd9902@naver.com>
2026-03-09 16:19:51 +0900

    feat: add Button component
    * feat: add common Button component (#31)
    
    * refactor: separate button styles and apply cva variant types (#31)
    
    * refactor: remove icon-related logic from Button component (#31)

---
+++ src/components/common/button/button.styles.ts
@@ -0,0 +1,34 @@
+import { cva } from 'class-variance-authority'
+
+export const buttonVariants = cva(
+  `inline-flex items-center justify-center font-semibold transition-colors disabled:cursor-not-allowed disabled:pointer-events-none disabled:bg-surface-disabled disabled:text-text-disabled`,
+  {
+    variants: {
+      variant: {
+        primary:
+          'bg-primary text-white hover:bg-primary-hover active:bg-primary-active',
+        outline:
+          'border border-primary text-primary bg-primary-100 disabled:text-text-sub disabled:border disabled:border-border-line',
+        text: 'bg-transparent text-text-main hover:bg-surface-sub',
+        textAccent:
+          'bg-transparent text-primary hover:bg-primary-100 hover:font-bold',
+        textMuted:
+          'bg-transparent text-text-sub hover:bg-gray-200 active:bg-primary-100 active:text-primary',
+      },
+      size: {
+        sm: 'px-5 py-2.5',
+        md: 'px-6 py-3',
+        lg: 'px-9 py-4',
+      },
+      rounded: {
+        md: 'rounded',
+        full: 'rounded-full',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'lg',
+      rounded: 'md',
+    },
+  }
+)
